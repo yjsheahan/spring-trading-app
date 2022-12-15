2cd1cc72cf9564b19ad820c049fc723110f503d1
yjsheahan <y.jsheahan@gmail.com>
2022-12-15 09:55:44 +0000

    sell service logic added

--- trading-app/src/main/java/com/kok/services/SellServiceImpl.java
+++ trading-app/src/main/java/com/kok/services/SellServiceImpl.java
@@ -15,19 +15,36 @@ public class SellServiceImpl implements SellService {
     @Override
     public Transaction createAsk(Transaction transaction, String nasdaq) {
         transaction.setNasdaq(nasdaq);
-<<<<<<< HEAD
-
+        transaction.setType("sell");
         List<Transaction> buyList = this.sellRepository.findAllByTypeAndStockOrderByPrice(nasdaq, "buy");
         for(Transaction t : buyList){
-            if(t.getSize() >= transaction.getSize()) t.setSize(t.getSize() - transaction.getSize());
-            else{
-                transaction.setSize(transaction.getSize() - t.getSize());
-                t.setSize(0);
+            // If buy price is greater than or equal to sell price
+            if(t.getPrice() >=  transaction.getPrice()) {
+                int newSize;
+                // Check if sizes are equal
+                if(t.getSize() == transaction.getSize()) {
+                    this.sellRepository.delete(t);
+                    this.sellRepository.delete(transaction);
+                    // TODO: Update order history of users with both transactions fully filled
+                    return transaction;
+                // If sell size is larger than pending transactions buy size
+                } else if(t.getSize() < transaction.getSize()) {
+                    // Set transaction size to the difference
+                    newSize = transaction.getSize() - t.getSize();
+                    transaction.setSize(newSize);
+                    // Delete pending transaction
+                    this.sellRepository.delete(t);
+                    // TODO: Update user's order history table with buy transaction fully filled, other partially filled
+                } else {
+                  // If transaction sell size is less than pending transactions buy size, set pending transaction size to difference
+                  newSize = t.getSize() - transaction.getSize();
+                  t.setSize(newSize);
+                  // Delete transaction
+                  this.sellRepository.delete(transaction);
+                  // TODO: Update order history of users with transaction fully filled, other partially filled
+                }
             }
         }
-=======
-        transaction.setType("sell");
->>>>>>> main
         return this.sellRepository.save(transaction);
     }
 
@@ -37,9 +54,8 @@ public class SellServiceImpl implements SellService {
     }
 
     @Override
-    public List<Transaction> findAllStockSellTransactions(String stock, String type) {
-        return this.sellRepository.findAllByTypeAndStockOrderByPrice(stock, type);
+    public List<Transaction> findAllStockSellTransactions(String nasdaq, String type) {
+        return this.sellRepository.findAllByTypeAndStockOrderByPrice(nasdaq, type);
     }
-
 
 }
