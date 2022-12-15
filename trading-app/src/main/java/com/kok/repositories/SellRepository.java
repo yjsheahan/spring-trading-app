96deb4c189f7d274e68b5a2548e6896008f15473
yjsheahan <y.jsheahan@gmail.com>
2022-12-15 08:57:00 +0000

    attempt at adding sell service logic

--- trading-app/src/main/java/com/kok/repositories/SellRepository.java
+++ trading-app/src/main/java/com/kok/repositories/SellRepository.java
@@ -2,9 +2,18 @@ package com.kok.repositories;
 
 import com.kok.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SellRepository extends JpaRepository<Transaction, Integer> {
+    @Query("select t from Transaction t where t.type = :type order by price DESC")
+    public List<Transaction> findAllByParamOrderByPrice(@Param("type") String type);
+
+    @Query("select t from Transaction t where t.type = :type and t.nasdaq = :nasdaq order by price DESC")
+    public List<Transaction> findAllByTypeAndStockOrderByPrice(@Param("nasdaq") String nasdaq, @Param("type") String type);
 
 }
--- trading-app/src/main/java/com/kok/services/SellServiceImpl.java
+++ trading-app/src/main/java/com/kok/services/SellServiceImpl.java
@@ -5,6 +5,8 @@ import com.kok.repositories.SellRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SellServiceImpl implements SellService {
     @Autowired
@@ -13,7 +15,26 @@ public class SellServiceImpl implements SellService {
     @Override
     public Transaction createAsk(Transaction transaction, String nasdaq) {
         transaction.setNasdaq(nasdaq);
+
+        List<Transaction> buyList = this.sellRepository.findAllByTypeAndStockOrderByPrice(nasdaq, "buy");
+        for(Transaction t : buyList){
+            if(t.getSize() >= transaction.getSize()) t.setSize(t.getSize() - transaction.getSize());
+            else{
+                transaction.setSize(transaction.getSize() - t.getSize());
+                t.setSize(0);
+            }
+        }
         return this.sellRepository.save(transaction);
+    }
+
+    @Override
+    public List<Transaction> findAllSellTransactions() {
+        return this.sellRepository.findAllByParamOrderByPrice("sell");
+    }
+
+    @Override
+    public List<Transaction> findAllStockSellTransactions(String stock, String type) {
+        return this.sellRepository.findAllByTypeAndStockOrderByPrice(stock, type);
     }
 
 
